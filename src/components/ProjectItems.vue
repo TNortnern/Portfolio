07fe51522d5b298a9988d6f4772ed2fc62d0e0a5
TNORTHERN <traynorthern@yahoo.com>
2020-01-11 08:40:26 -0600

    Refactoring, moving project items into a component

---
+++ src/components/ProjectItems.vue
@@ -0,0 +1,149 @@
+<template>
+  <div class="project-items">
+        
+      <div
+        v-for="(project, key) in projects"
+        :key="key"
+        class="card"
+        style="width: 18rem;"
+      >
+        <div class="card-image">
+          <img :src="project.images[0]" class="card-img-top" alt="..." />
+          <div class="card-action" style="">Click to View</div>
+        </div>
+        <div class="card-body">
+          <h5 class="card-title">{{ project.name }}</h5>
+          <p class="card-text">
+            {{ project.description }}
+          </p>
+        </div>
+      </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data: () => ({
+         projects: [
+      {
+        name: "Northern's Factory",
+        description: "lorem ipsum",
+        images: [
+          "https://www.clientresourcesinc.com/wp-content/uploads/2018/06/vuejs_logo.png",
+          "https://vuejsexamples.com/content/images/2017/10/Vue-Awesome.gif"
+        ],
+        technologies: ["PHP", "HTML/CSS", "Vanilla JavaScript"]
+      },
+      {
+        name: "Northern's Factory",
+        description: "lorem ipsum",
+        images: [
+          "https://www.clientresourcesinc.com/wp-content/uploads/2018/06/vuejs_logo.png",
+          "https://vuejsexamples.com/content/images/2017/10/Vue-Awesome.gif"
+        ],
+        technologies: ["PHP", "HTML/CSS", "Vanilla JavaScript"]
+      },
+      {
+        name: "Northern's Factory",
+        description: "lorem ipsum",
+        images: [
+          "https://www.clientresourcesinc.com/wp-content/uploads/2018/06/vuejs_logo.png",
+          "https://vuejsexamples.com/content/images/2017/10/Vue-Awesome.gif"
+        ],
+        technologies: ["PHP", "HTML/CSS", "Vanilla JavaScript"]
+      },
+      {
+        name: "Northern's Factory",
+        description: "lorem ipsum",
+        images: [
+          "https://www.clientresourcesinc.com/wp-content/uploads/2018/06/vuejs_logo.png",
+          "https://vuejsexamples.com/content/images/2017/10/Vue-Awesome.gif"
+        ],
+        technologies: ["PHP", "HTML/CSS", "Vanilla JavaScript"]
+      },
+      {
+        name: "Northern's Factory",
+        description: "lorem ipsum",
+        images: [
+          "https://www.clientresourcesinc.com/wp-content/uploads/2018/06/vuejs_logo.png",
+          "https://vuejsexamples.com/content/images/2017/10/Vue-Awesome.gif"
+        ],
+        technologies: ["PHP", "Laravel", "Vanilla JavaScript"]
+      },
+      {
+        name: "Northern's Factory",
+        description: "lorem ipsum",
+        images: [
+          "https://www.clientresourcesinc.com/wp-content/uploads/2018/06/vuejs_logo.png",
+          "https://vuejsexamples.com/content/images/2017/10/Vue-Awesome.gif"
+        ],
+        technologies: ["PHP", "HTML/CSS", "Vanilla JavaScript"]
+      },
+      {
+        name: "Northern's Factory",
+        description: "lorem ipsum",
+        images: [
+          "https://www.clientresourcesinc.com/wp-content/uploads/2018/06/vuejs_logo.png",
+          "https://vuejsexamples.com/content/images/2017/10/Vue-Awesome.gif"
+        ],
+        technologies: ["PHP", "Vanilla JavaScript"]
+      },
+      {
+        name: "Northern's Factory",
+        description: "lorem ipsum",
+        images: [
+          "https://www.clientresourcesinc.com/wp-content/uploads/2018/06/vuejs_logo.png",
+          "https://vuejsexamples.com/content/images/2017/10/Vue-Awesome.gif"
+        ],
+        technologies: ["Vanilla PHP", "HTML/CSS", "Vanilla JavaScript"]
+      },
+      {
+        name: "Northern's Factory",
+        description: "lorem ipsum",
+        images: [
+          "https://www.clientresourcesinc.com/wp-content/uploads/2018/06/vuejs_logo.png",
+          "https://vuejsexamples.com/content/images/2017/10/Vue-Awesome.gif"
+        ],
+        technologies: ["PHP", "HTML/CSS", "Vanilla JavaScript"]
+      }
+    ],
+    })
+}
+</script>
+
+<style lang="scss" scoped>
+@import "../styles";
+.project-items {
+  display: flex;
+  flex-wrap: wrap;
+  img {
+    width: 100%;
+  }
+}
+img {
+  height: 100px;
+  width: 100px;
+}
+.card-image {
+  &:hover {
+    .card-action {
+      opacity: 1;
+    }
+  }
+}
+.card-action {
+  background-color: rgba(20, 20, 20, 0.596);
+  height: 100%;
+  position: absolute;
+  width: 100%;
+  top: 0%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  transition: $default-timing;
+  opacity: 0;
+  color: white;
+  font-size: 20px;
+  cursor: pointer;
+}
+</style>
--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -1,175 +1,26 @@
 <template>
   <div class="projects">
     <FilterLanguages />
-    <!-- {{included}} -->
-    <!-- {{ filteredProjects }} -->
-    <!-- <button class="button button-primary" @click="checkProjs">check projs</button> -->
-    <div class="project-items">
-        
-      <div
-        v-for="(project, key) in projects"
-        :key="key"
-        class="card"
-        style="width: 18rem;"
-      >
-        <div class="card-image">
-          <img :src="project.images[0]" class="card-img-top" alt="..." />
-          <div class="card-action" style="">Click to View</div>
-        </div>
-        <div class="card-body">
-          <h5 class="card-title">{{ project.name }}</h5>
-          <p class="card-text">
-            {{ project.description }}
-          </p>
-        </div>
-      </div>
-    </div>
+    <ProjectItems />
   </div>
 </template>
 
 <script>
 /* eslint-disable no-console */
 import FilterLanguages from "./FilterLanguages";
-
+import ProjectItems from './ProjectItems';
 export default {
   components: {
-    FilterLanguages
+    FilterLanguages,
+    ProjectItems
   },
   data: () => ({
-    languages: [
-      { name: "HTML/CSS", image: "/" },
-      { name: "Vanilla JavaScript", image: "/" },
-      { name: "Vanilla PHP", image: "/" },
-      { name: "Laravel", image: "/" },
-      { name: "Vue", image: "/" },
-      { name: "React", image: "/" },
-      { name: "React Native", image: "/" },
-      { name: "SCSS", image: "/" }
-    ],
-    projects: [
-      {
-        name: "Northern's Factory",
-        description: "lorem ipsum",
-        images: [
-          "https://www.clientresourcesinc.com/wp-content/uploads/2018/06/vuejs_logo.png",
-          "https://vuejsexamples.com/content/images/2017/10/Vue-Awesome.gif"
-        ],
-        technologies: ["PHP", "HTML/CSS", "Vanilla JavaScript"]
-      },
-      {
-        name: "Northern's Factory",
-        description: "lorem ipsum",
-        images: [
-          "https://www.clientresourcesinc.com/wp-content/uploads/2018/06/vuejs_logo.png",
-          "https://vuejsexamples.com/content/images/2017/10/Vue-Awesome.gif"
-        ],
-        technologies: ["PHP", "HTML/CSS", "Vanilla JavaScript"]
-      },
-      {
-        name: "Northern's Factory",
-        description: "lorem ipsum",
-        images: [
-          "https://www.clientresourcesinc.com/wp-content/uploads/2018/06/vuejs_logo.png",
-          "https://vuejsexamples.com/content/images/2017/10/Vue-Awesome.gif"
-        ],
-        technologies: ["PHP", "HTML/CSS", "Vanilla JavaScript"]
-      },
-      {
-        name: "Northern's Factory",
-        description: "lorem ipsum",
-        images: [
-          "https://www.clientresourcesinc.com/wp-content/uploads/2018/06/vuejs_logo.png",
-          "https://vuejsexamples.com/content/images/2017/10/Vue-Awesome.gif"
-        ],
-        technologies: ["PHP", "HTML/CSS", "Vanilla JavaScript"]
-      },
-      {
-        name: "Northern's Factory",
-        description: "lorem ipsum",
-        images: [
-          "https://www.clientresourcesinc.com/wp-content/uploads/2018/06/vuejs_logo.png",
-          "https://vuejsexamples.com/content/images/2017/10/Vue-Awesome.gif"
-        ],
-        technologies: ["PHP", "Laravel", "Vanilla JavaScript"]
-      },
-      {
-        name: "Northern's Factory",
-        description: "lorem ipsum",
-        images: [
-          "https://www.clientresourcesinc.com/wp-content/uploads/2018/06/vuejs_logo.png",
-          "https://vuejsexamples.com/content/images/2017/10/Vue-Awesome.gif"
-        ],
-        technologies: ["PHP", "HTML/CSS", "Vanilla JavaScript"]
-      },
-      {
-        name: "Northern's Factory",
-        description: "lorem ipsum",
-        images: [
-          "https://www.clientresourcesinc.com/wp-content/uploads/2018/06/vuejs_logo.png",
-          "https://vuejsexamples.com/content/images/2017/10/Vue-Awesome.gif"
-        ],
-        technologies: ["PHP", "Vanilla JavaScript"]
-      },
-      {
-        name: "Northern's Factory",
-        description: "lorem ipsum",
-        images: [
-          "https://www.clientresourcesinc.com/wp-content/uploads/2018/06/vuejs_logo.png",
-          "https://vuejsexamples.com/content/images/2017/10/Vue-Awesome.gif"
-        ],
-        technologies: ["Vanilla PHP", "HTML/CSS", "Vanilla JavaScript"]
-      },
-      {
-        name: "Northern's Factory",
-        description: "lorem ipsum",
-        images: [
-          "https://www.clientresourcesinc.com/wp-content/uploads/2018/06/vuejs_logo.png",
-          "https://vuejsexamples.com/content/images/2017/10/Vue-Awesome.gif"
-        ],
-        technologies: ["PHP", "HTML/CSS", "Vanilla JavaScript"]
-      }
-      //   {
-      //     name: "Portfolio1",
-      //     description: "lorem ipsum",
-      //     images: ["img1, img2"],
-      //     technologies: ["VueJS", "HTML/CSS", "SCSS"]
-      //   }
-    ],
     included: []
   }),
   methods: {
     removeAsFilter(language) {
       this.included = this.included.filter(lang => lang.name != language.name);
     },
-    checkProjs () {
-        const projs = this.included.forEach(i => {
-            // const found = .some(r=> arr2.indexOf(r) >= 0)
-            // console.log(found)
-            // console.log(p.technologies)
-        //    if (i.name(t => this.included.includes(t))) {
-        //        return i
-        //    }
-        this.projects.forEach(p => {
-            p.technologies.forEach(t => {
-                if (t == i.name) {
-                    return p
-                }
-            })
-        });
-       })
-       return projs
-    }
-  },
-  computed: {
-    filteredProjects() {
-    //    let items = []
-       const projs = this.projects.forEach(p => {
-           if (p.technologies.some(t => this.included.includes(t))) {
-               return p
-           }
-       })
-       return projs
-    }
   }
 };
 </script>
@@ -180,37 +31,5 @@ export default {
   display: grid;
   grid-template-columns: 20% 80%;
 }
-.project-items {
-  display: flex;
-  flex-wrap: wrap;
-  img {
-    width: 100%;
-  }
-}
-img {
-  height: 100px;
-  width: 100px;
-}
-.card-image {
-  &:hover {
-    .card-action {
-      opacity: 1;
-    }
-  }
-}
-.card-action {
-  background-color: rgba(20, 20, 20, 0.596);
-  height: 100%;
-  position: absolute;
-  width: 100%;
-  top: 0%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  transition: $default-timing;
-  opacity: 0;
-  color: white;
-  font-size: 20px;
-  cursor: pointer;
-}
+
 </style>
